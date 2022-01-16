67f605e627eb5c44612d5b35cbeac1f2f3fe545f
akashprakas <akashp805@gmail.com>
2022-01-16 13:29:45 +0530

    lesson 1 completed

--- src/environment.cpp
+++ src/environment.cpp
@@ -44,6 +44,12 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // RENDER OPTIONS
     bool renderScene = true;
     std::vector<Car> cars = initHighway(renderScene, viewer);
+    Lidar* lidar = new Lidar(cars,0.0);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = lidar->scan();
+
+    // renderRays( viewer,lidar->position, cloud);
+
+    renderPointCloud(viewer,cloud, "point cloud");
     
     // TODO:: Create lidar sensor 
 
